bf4b0f6ab90a80b32e183e9e9b79b558a722014e
lyxmq.ljh <lyxmq.ljh@9c0ba882-c8bc-11dd-9042-6d746b85d38b>
2013-03-15 11:25:26 +0000

    Form的构建

--- istyle-app/istyle-commons/src/main/java/com/toney/istyle/form/ProductForm.java
+++ istyle-app/istyle-commons/src/main/java/com/toney/istyle/form/ProductForm.java
@@ -40,8 +40,9 @@ public class ProductForm extends BaseObject {
 	private Date lastUpdate;
 
 	private Short status;
-	
-	private String mainImage;
+
+	private ProductPicForm productPicForm;
+	private ProductStatForm productStatForm;
 
 	/**
 	 * 商品的URL
@@ -51,7 +52,7 @@ public class ProductForm extends BaseObject {
 	/**
 	 * 类目
 	 */
-	private Integer catCode;
+	private CategoryForm categoryForm;
 
 	public Long getId() {
 		return id;
@@ -133,35 +134,44 @@ public class ProductForm extends BaseObject {
 		this.productUrl = productUrl;
 	}
 
-	public Integer getCatCode() {
-		return catCode;
+	public CategoryForm getCategoryForm() {
+		return categoryForm;
 	}
 
-	public void setCatCode(Integer catCode) {
-		this.catCode = catCode;
-	}
-	
-	public String getMainImage() {
-		return mainImage;
+	public void setCategoryForm(CategoryForm categoryForm) {
+		this.categoryForm = categoryForm;
 	}
 
-	public void setMainImage(String mainImage) {
-		this.mainImage = mainImage;
+	public ProductPicForm getProductPicForm() {
+		return productPicForm;
+	}
+
+	public void setProductPicForm(ProductPicForm productPicForm) {
+		this.productPicForm = productPicForm;
+	}
+
+	public ProductStatForm getProductStatForm() {
+		return productStatForm;
+	}
+
+	public void setProductStatForm(ProductStatForm productStatForm) {
+		this.productStatForm = productStatForm;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((catCode == null) ? 0 : catCode.hashCode());
+		result = prime * result + ((categoryForm == null) ? 0 : categoryForm.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((lastUpdate == null) ? 0 : lastUpdate.hashCode());
-		result = prime * result + ((mainImage == null) ? 0 : mainImage.hashCode());
 		result = prime * result + ((onsale == null) ? 0 : onsale.hashCode());
 		result = prime * result + ((platformCode == null) ? 0 : platformCode.hashCode());
 		result = prime * result + ((platformName == null) ? 0 : platformName.hashCode());
 		result = prime * result + ((produceCode == null) ? 0 : produceCode.hashCode());
 		result = prime * result + ((productName == null) ? 0 : productName.hashCode());
+		result = prime * result + ((productPicForm == null) ? 0 : productPicForm.hashCode());
+		result = prime * result + ((productStatForm == null) ? 0 : productStatForm.hashCode());
 		result = prime * result + ((productUrl == null) ? 0 : productUrl.hashCode());
 		result = prime * result + ((status == null) ? 0 : status.hashCode());
 		result = prime * result + ((userName == null) ? 0 : userName.hashCode());
@@ -177,10 +187,10 @@ public class ProductForm extends BaseObject {
 		if (getClass() != obj.getClass())
 			return false;
 		ProductForm other = (ProductForm) obj;
-		if (catCode == null) {
-			if (other.catCode != null)
+		if (categoryForm == null) {
+			if (other.categoryForm != null)
 				return false;
-		} else if (!catCode.equals(other.catCode))
+		} else if (!categoryForm.equals(other.categoryForm))
 			return false;
 		if (id == null) {
 			if (other.id != null)
@@ -191,11 +201,6 @@ public class ProductForm extends BaseObject {
 			if (other.lastUpdate != null)
 				return false;
 		} else if (!lastUpdate.equals(other.lastUpdate))
-			return false;
-		if (mainImage == null) {
-			if (other.mainImage != null)
-				return false;
-		} else if (!mainImage.equals(other.mainImage))
 			return false;
 		if (onsale == null) {
 			if (other.onsale != null)
@@ -222,6 +227,16 @@ public class ProductForm extends BaseObject {
 				return false;
 		} else if (!productName.equals(other.productName))
 			return false;
+		if (productPicForm == null) {
+			if (other.productPicForm != null)
+				return false;
+		} else if (!productPicForm.equals(other.productPicForm))
+			return false;
+		if (productStatForm == null) {
+			if (other.productStatForm != null)
+				return false;
+		} else if (!productStatForm.equals(other.productStatForm))
+			return false;
 		if (productUrl == null) {
 			if (other.productUrl != null)
 				return false;
@@ -243,8 +258,8 @@ public class ProductForm extends BaseObject {
 	@Override
 	public String toString() {
 		return "ProductForm [id=" + id + ", produceCode=" + produceCode + ", platformCode=" + platformCode + ", platformName=" + platformName + ", productName=" + productName
-				+ ", onsale=" + onsale + ", userName=" + userName + ", lastUpdate=" + lastUpdate + ", status=" + status + ", mainImage=" + mainImage + ", productUrl=" + productUrl
-				+ ", catCode=" + catCode + "]";
+				+ ", onsale=" + onsale + ", userName=" + userName + ", lastUpdate=" + lastUpdate + ", status=" + status + ", productPicForm=" + productPicForm
+				+ ", productStatForm=" + productStatForm + ", productUrl=" + productUrl + ", categoryForm=" + categoryForm + "]";
 	}
 
 }
