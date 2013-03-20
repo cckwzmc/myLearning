fb26830f3755883e8b517503255c2181f303f547
lyxmq.ljh <lyxmq.ljh@9c0ba882-c8bc-11dd-9042-6d746b85d38b>
2013-03-20 09:37:14 +0000

    提交代码

---
+++ istyle-app/istyle-commons/src/main/java/com/toney/istyle/form/SearchForm.java
@@ -0,0 +1,24 @@
+package com.toney.istyle.form;
+
+import java.io.Serializable;
+
+/**
+ *************************************************************** 
+ * <p>
+ * @CLASS :SearchForm.java
+ * @DESCRIPTION : 搜索Form
+ * @AUTHOR :toney.li
+ * @VERSION :v1.0
+ * @DATE :Mar 20, 2013
+ *       </p>
+ **************************************************************** 
+ */
+public class SearchForm implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7090549675636380160L;
+	
+
+}
